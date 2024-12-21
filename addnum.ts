02a11be33bbb0058f49d46ea93bf02cf0886e12f
harshavardhan1729 <60821548+harshavardhan1729@users.noreply.github.com>
2024-12-21 22:46:51 +0530

    Update addnum.ts

--- addnum.ts
+++ addnum.ts
@@ -1,39 +1,54 @@
-function addNumbers(numbersString: string): number {
-    if (typeof inputString !== "string") {
-        throw new Error("Input must be a string.");
+
+export class AddNumbersService {
+
+    constructor() { }
+  
+ 
+    addNumbers(inputString: string): number {
+      if (typeof inputString !== "string") {
+          throw new Error("Input must be a string.");
+      }
+  
+      if(this.checkNegative(inputString)){
+          throw new Error("Input cannot have negative integers");
+      }
+      else{
+  
+      const numberPattern = /\d+/g;
+  
+      const matches = inputString.match(numberPattern);
+      const matchednum=matches ? matches.map(Number) : [];
+      
+      return matchednum.reduce((sum, number) => sum + number, 0);
+      }
+      
+  
+  }
+  checkNegative(numbersString: string): boolean {
+      const pattern = /-\d+/g;
+  
+      const matchess = numbersString.match(pattern);
+  
+      return matchess!=null && matchess.length>0 ? true : false;   
+  }
+  
+  }
+
+  addNumbers("/n1,2;2");
+
+
+
+
+
+ function addNumbers(str:any) {
+    const inputString: string =str ;
+    let cl:any=new  AddNumbersService();
+    try{
+    const result: number = cl.addNumbers(inputString);
+    console.log("sum is",result); 
     }
-
-    if(checkNegative(numbersString)){
-        throw new Error("Input cannot have negative integers");
+    catch(e:any){
+    console.log(e.message);
     }
-    else{
-
-    const numberPattern = /\d+/g;
-
-    const matches = inputString.match(numberPattern);
-    const matchednum=matches ? matches.map(Number) : [];
-    
-    return matchednum.reduce((sum, number) => sum + number, 0);
-    }
-    
-
 }
-function checkNegative(numbersString: string): boolean {
-    const pattern = /-\d+/g;
-
-    const matchess = numbersString.match(pattern);
-
-    return matchess!=null && matchess.length>0 ? true : false; 
-}
-
-const inputString: string = "10,20,,30,40,abc";
-const delimiter: string = ",";
-try{
-const result: number = addNumbers(inputString);
-console.log("sum is",result); 
-}
-catch(e:any){
-console.log(e.message);
-}
-
 
