bcda4567a957c0e095a0a8befc26b19035f7dd88
harshavardhan1729 <60821548+harshavardhan1729@users.noreply.github.com>
2024-12-21 21:47:07 +0530

    ignoring strings other than numbers

--- addnum.ts
+++ addnum.ts
@@ -1,14 +1,23 @@
-function addNumbers(numbersString: string, delimiter: string = ","): number {
+function addNumbers(numbersString: string, delimiter: string = ",", ignore: boolean = false): number {
     if (typeof numbersString !== "string") {
         throw new Error("Input should be a string format");
     }
 
-   
+    if (!delimiter) {
+        throw new Error("delimiter cannot be empty.");
+    }
 
     const numbersArray: string[] = numbersString.split(delimiter); //split using delimter
 
     const numbers: number[] = numbersArray.map(num => {
         const parsednum: number = parseFloat(num.trim());
+        if (isNaN(parsednum)) {
+            if (ignore) {
+                return 0; 
+            } else {
+                throw new Error(`Invalid number: '${num}'`);
+            }
+        }
         return parsednum;
     });
 
@@ -16,8 +25,8 @@ function addNumbers(numbersString: string, delimiter: string = ","): number {
     return numbers.reduce((sum, number) => sum + number, 0);
 }
 
-const inputString: string = "10,20,30,40";
+const inputString: string = "10,20,30,40,abc";
 const delimiter: string = ",";
-const result: number = addNumbers(inputString, delimiter);
-console.log("sum =",result); 
+const result: number = addNumbers(inputString, delimiter,true);
+console.log("sum =",result);
 
